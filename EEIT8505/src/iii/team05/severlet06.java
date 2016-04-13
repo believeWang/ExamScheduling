16ef59a3a0972e1d93ea40676b921c6a631c1dc8
kidd <kidd@EEIT-PC>
2016-04-13 17:14:33 +0800

    新增out.printle

--- EEIT8505/src/iii/team05/severlet06.java
+++ EEIT8505/src/iii/team05/severlet06.java
@@ -30,6 +30,7 @@ public class severlet06 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Today is a badday");
+		out.println("我要修改");
 	}
 
 	/**
