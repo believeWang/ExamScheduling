0d75925b150c096e46854ef1061e6f37eb04d059
Juni <Juni@gmail.com>
2016-04-14 15:00:16 +0800

    first commit

---
+++ ExamScheduling/src/main/java/iii/EEIT8505/ExamScheduling/App.java
@@ -0,0 +1,13 @@
+package iii.EEIT8505.ExamScheduling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
