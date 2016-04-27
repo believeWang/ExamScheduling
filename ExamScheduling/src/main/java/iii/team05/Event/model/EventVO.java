9d3a5b6bb7c796417821ecfc94cb820531c7c19a
kao <kmhlcc@gmail.com>
2016-04-27 17:20:03 +0800

    新增 event section job 模組
    fullcalendar fancybox bootstrap 套件引入

---
+++ ExamScheduling/src/main/java/iii/team05/Event/model/EventVO.java
@@ -0,0 +1,79 @@
+package iii.team05.Event.model;
+/*
+ drop table Event;
+
+ create table Event_Exist (
+	 eventid             int IDENTITY (1,1) not null,
+	 empno               int not null,
+	 starttime           datetime not null,
+	 endtime             datetime not null,
+	 bgcolor             varchar(20) null,
+	 title               varchar(20) not null,
+	 ecno                varchar(30) not null,
+	 eventremark         text null,
+ 
+	 --constraint Event_erid_fk foreign key (erid) references Examiner (erid),  --
+	 --constraint Event_ecno_fk foreign key (ecno) references Examinee_Cat (ecno),  --
+	 constraint Event_eventid_pk primary key (eventid)
+ );
+*/
+public class EventVO  implements java.io.Serializable{
+	private Integer eventid;
+	private Integer empno;
+	private java.sql.Timestamp starttime;
+	private java.sql.Timestamp endtime;
+	private String bgcolor;
+	private String title;
+	private String ecno;
+	private String eventremark;
+	
+	public Integer getEventid() {
+		return eventid;
+	}
+	public void setEventid(Integer eventid) {
+		this.eventid = eventid;
+	}
+	public Integer getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+	public java.sql.Timestamp getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(java.sql.Timestamp starttime) {
+		this.starttime = starttime;
+	}
+	public java.sql.Timestamp getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(java.sql.Timestamp endtime) {
+		this.endtime = endtime;
+	}
+	public String getBgcolor() {
+		return bgcolor;
+	}
+	public void setBgcolor(String bgcolor) {
+		this.bgcolor = bgcolor;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getEcno() {
+		return ecno;
+	}
+	public void setEcno(String ecno) {
+		this.ecno = ecno;
+	}
+	public String getEventremark() {
+		return eventremark;
+	}
+	public void setEventremark(String eventremark) {
+		this.eventremark = eventremark;
+	}
+	
+}
---
+++ ExamScheduling/src/main/java/iii/team05/Section/model/SectionDAO.java
@@ -0,0 +1,269 @@
+package iii.team05.Section.model;
+
+import java.util.*;
+import java.sql.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import iii.team05.Section.model.SectionVO;
+
+public class SectionDAO implements SectionDAO_interface {
+
+	// 一個應用程式中,針對一個資料庫 ,共用一個DataSource即可
+	private static DataSource ds = null;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/BlobClobDEMO");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static final String INSERT_STMT = "INSERT INTO Section (sectiontime, sectionStatus) VALUES (?, ?)";
+	private static final String GET_ALL_STMT = "SELECT sectiontime, sectionStatus FROM Section";
+	private static final String GET_ONE_STMT = "SELEC sectiontime, sectionStatus FROM Section where sectiontime = ?";
+	private static final String DELETE_STMT = "DELETE FROM Section where sectiontime = ?";
+	private static final String UPDATE_STMT = "UPDATE Section set sectionStatus=? where sectiontime = ?";
+
+	@Override
+	public void insert(SectionVO sectionVO) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(INSERT_STMT);
+
+			pstmt.setTime(1, sectionVO.getSectiontime());
+			pstmt.setDouble(2, sectionVO.getSectionStatus());
+
+			pstmt.executeUpdate();
+
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void update(SectionVO sectionVO) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(UPDATE_STMT);
+			
+			pstmt.setDouble(1, sectionVO.getSectionStatus());
+			pstmt.setTime(2, sectionVO.getSectiontime());
+
+			pstmt.executeUpdate();
+
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void delete(java.sql.Time sectiontime) {
+		int updateCount_EMPs = 0;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = ds.getConnection();
+			
+			pstmt = con.prepareStatement(DELETE_STMT);
+			pstmt.setTime(1, sectiontime);
+			updateCount_EMPs = pstmt.executeUpdate();
+			
+			System.out.println("刪除事件id＝" + sectiontime +"有"+ updateCount_EMPs
+					+ "筆被刪除");
+			
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			if (con != null) {
+				try {
+					// 3●設定於當有exception發生時之catch區塊內
+					con.rollback();
+				} catch (SQLException excep) {
+					throw new RuntimeException("rollback error occured. "
+							+ excep.getMessage());
+				}
+			}
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public SectionVO findByPrimaryKey(java.sql.Time sectiontime) {
+
+		SectionVO sectionVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+			
+			pstmt.setTime(1, sectiontime);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				
+				sectionVO = new SectionVO();
+				sectionVO.setSectiontime(rs.getTime("sectiontime"));
+				sectionVO.setSectionStatus(rs.getDouble("sectionStatus"));
+				
+			}
+
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return sectionVO;
+	}
+
+	@Override
+	public List<SectionVO> getAll() {
+		List<SectionVO> list = new ArrayList<SectionVO>();
+		SectionVO sectionVO = null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				sectionVO = new SectionVO();
+				sectionVO.setSectiontime(rs.getTime("sectiontime"));
+				sectionVO.setSectionStatus(rs.getDouble("sectionStatus"));
+				list.add(sectionVO); // Store the row in the list
+			}
+
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
+	}
+
+}
