622d555b43933f7de25f480e6d75a9cae91eebdf
Juni <Juni@gmail.com>
2016-05-13 16:16:53 +0800

    編輯CKeditor的config.js

--- ExamScheduling/src/main/webapp/ckeditor/config.js
+++ ExamScheduling/src/main/webapp/ckeditor/config.js
@@ -7,6 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.filebrowserImageUploadUrl = './upload.php?type=img'; 
+    config.filebrowserFlashUploadUrl = './upload.php?type=flash';
+	config.allowedContent=true;
 	config.toolbar = 'xxxx';
 	config.toolbar_Basic =
 		 [
