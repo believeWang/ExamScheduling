6997ef3ec0cfa8a4b438d09c40ddfa0b17ecbbae
Steven <Steven@gmail.com>
2016-05-25 17:43:07 +0800

    主考官 HEADER初版

---
+++ ExamScheduling/src/main/java/iii/team05/gmail_login/Login.java
@@ -0,0 +1,37 @@
+package iii.team05.gmail_login;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MesServlet
+ */
+@WebServlet("/gmail_login/login.do")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Login() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+			String login="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=14162985850-ukk7ka9npfvhatl0vi7mccqgnhnq2ui0.apps.googleusercontent.com&redirect_uri=http://localhost:8081/ExamScheduling/GoogleCheck&scope=https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/admin.directory.resource.calendar&access_type=offline";
+		response.sendRedirect(response.encodeRedirectURL(login));
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+	
+
+}
+}
--- ExamScheduling/src/main/webapp/mes/ListNotReplyMessage.jsp
+++ ExamScheduling/src/main/webapp/mes/ListNotReplyMessage.jsp
@@ -13,23 +13,10 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>考試預約系統Backstage</title>
-		<style>
-       @import url('<%=request.getContextPath()%>/mes/css/main.css');
-		</style>
 </head>
 <body>
 
-     	<header>
- 		       		<img src="<%=request.getContextPath()%>/mes/images/logo.png" style="float:left" />  
-
- 		 <nav>
-       		<ul id="menu">	   
-       	  	      <li><a href="<%=request.getContextPath()%>/index.jsp">輸入成績</a></li>
-       		      <li><a href="<%=request.getContextPath()%>/index.jsp">編輯考題</a></li>
-		  	      <li><a href="<%=request.getContextPath()%>/mes/ListNotReplyMessage.jsp">留言板</a></li>
-       		</ul>
-       	</nav>       	
-       </header>
+     <%@ include file="header.jsp" %>
 
 
 
